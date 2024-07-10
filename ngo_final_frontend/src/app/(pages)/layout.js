0eb05d022f5ff3edbdb8b7d9b079d75d321b27bd
Hardiksaresaoc <163412905+Hardiksaresaoc@users.noreply.github.com>
2024-07-10 19:05:45 +0530

    Update layout.js

--- ngo_final_frontend/src/app/(pages)/layout.js
+++ ngo_final_frontend/src/app/(pages)/layout.js
@@ -3,33 +3,39 @@ import "../globals.css";
 import Footer from "@/component/footer";
 import { Inter } from "next/font/google";
 import Loading from "../loading";
-import NotFound from "../not-found";  // Adjust import name to follow PascalCase convention
-import NoFundraiser from "@/component/nofundraiser";  // Adjust import name to follow PascalCase convention
+import notFound from "../not-found";
+import Notfundraiser from "@/component/nofundraiser";
 
 export const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  const isLoading = !children && !NotFound && !NoFundraiser;
-
   return (
     <>
-      <html lang="en">
+      <html>
         <head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-3F9DGFXBH4"></script>
+       <script async src="https://www.googletagmanager.com/gtag/js?id=G-3F9DGFXBH4"></script>
+
           <script>
+
             {`
+
               window.dataLayer = window.dataLayer || [];
+
               function gtag(){dataLayer.push(arguments);}
+
               gtag('js', new Date());
+
               gtag('config', 'G-3F9DGFXBH4');
+
             `}
+
           </script>
           <link rel="icon" href="/images/favicon.ico" />
         </head>
         <body className={inter.className}>
           <Header />
           <div style={{ minHeight: "70vh" }}>
-            {isLoading ? <Loading /> : children}
+            {!children && !notFound && !Notfundraiser ? <Loading /> : children}
           </div>
           <Footer />
         </body>
