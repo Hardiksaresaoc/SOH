c1be633796a6500dd5dcd428860f0723cbe8c4ab
Hardiksaresaoc <163412905+Hardiksaresaoc@users.noreply.github.com>
2024-07-10 16:58:30 +0530

    Update page.js

--- ngo_final_frontend/src/app/(pages)/summary/page.js
+++ ngo_final_frontend/src/app/(pages)/summary/page.js
@@ -287,23 +287,30 @@ export default function Page() {
 
   return (
     <>
-     {
-        <>
-          <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-3F9DGFXBH4"
-          ></Script>
-          <Script>
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+     // {
+     //    <>
+     //      <Script
+     //        async
+     //        src="https://www.googletagmanager.com/gtag/js?id=G-3F9DGFXBH4"
+     //      ></Script>
+     //      <Script>
+     //        {`
+     //        window.dataLayer = window.dataLayer || [];
+     //        function gtag(){dataLayer.push(arguments);}
+     //        gtag('js', new Date());
 
-            gtag('config', 'G-3F9DGFXBH4');
-          `}
-          </Script>
-        </>
-      }
+     //        gtag('config', 'G-3F9DGFXBH4');
+     //      `}
+     //      </Script>
+     //    </>
+     //  }
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "n55horgc3p");
+</script>
       <main className={styles.mainPage}>
         <div className={"container"}>
           <div className={styles.upperPortion}>
