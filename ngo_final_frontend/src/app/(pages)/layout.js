376d9497c6015fbb8acf31c77d973b67ecbc29cf
Hardiksaresaoc <163412905+Hardiksaresaoc@users.noreply.github.com>
2024-07-10 19:00:20 +0530

    Update layout.js

--- ngo_final_frontend/src/app/(pages)/layout.js
+++ ngo_final_frontend/src/app/(pages)/layout.js
@@ -3,30 +3,33 @@ import "../globals.css";
 import Footer from "@/component/footer";
 import { Inter } from "next/font/google";
 import Loading from "../loading";
-import notFound from "../not-found";
-import Notfundraiser from "@/component/nofundraiser";
+import NotFound from "../not-found";  // Adjust import name to follow PascalCase convention
+import NoFundraiser from "@/component/nofundraiser";  // Adjust import name to follow PascalCase convention
 
 export const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const isLoading = !children && !NotFound && !NoFundraiser;
+
   return (
     <>
-      <html>
+      <html lang="en">
         <head>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3F9DGFXBH4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-3F9DGFXBH4');
-</script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-3F9DGFXBH4"></script>
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3F9DGFXBH4');
+            `}
+          </script>
           <link rel="icon" href="/images/favicon.ico" />
         </head>
         <body className={inter.className}>
           <Header />
           <div style={{ minHeight: "70vh" }}>
-            {!children && !notFound && !Notfundraiser ? <Loading /> : children}
+            {isLoading ? <Loading /> : children}
           </div>
           <Footer />
         </body>
