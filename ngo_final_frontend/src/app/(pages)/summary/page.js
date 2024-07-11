616aaece6146005519c5ef358738dcddc9402af1
hardik <hardiksaresa.oc@gmail.com>
2024-07-11 17:14:31 +0530

    microsoft

--- ngo_final_frontend/src/app/(pages)/summary/page.js
+++ ngo_final_frontend/src/app/(pages)/summary/page.js
@@ -14,7 +14,6 @@ import Swal from "sweetalert2";
 import { showSwal } from "@/validation";
 import Script from "next/script";
 
-
 export default function Page() {
   const images = [
     "/images/ChiefMinisterOfAssam.jpeg",
@@ -287,20 +286,17 @@ export default function Page() {
 
   return (
     <>
-     {
+      {
         <>
-          <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-3F9DGFXBH4"
-          ></Script>
+          <Script async src="https://www.clarity.ms/tag/n5o3v6zlha"></Script>
           <Script>
             {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-3F9DGFXBH4');
-          `}
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "n5o3v6zlha");
+      `}
           </Script>
         </>
       }
