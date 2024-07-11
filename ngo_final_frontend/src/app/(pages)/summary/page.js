cc6b54611016c94b35637f5419777a1da8bbe42f
hardik <hardiksaresa.oc@gmail.com>
2024-07-11 17:40:08 +0530

    google extension

--- ngo_final_frontend/src/app/(pages)/summary/page.js
+++ ngo_final_frontend/src/app/(pages)/summary/page.js
@@ -288,15 +288,18 @@ export default function Page() {
     <>
       {
         <>
-          {/* <Script async src="https://www.clarity.ms/tag/n5o3v6zlha"></Script> */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-3F9DGFXBH4"
+          ></Script>
           <Script>
             {`
-      (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "n5o3v6zlha");
-      `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3F9DGFXBH4');
+          `}
           </Script>
         </>
       }
