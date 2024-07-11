151f444a07484f7f3b18336d0e7299259e1e7f41
hardik <hardiksaresa.oc@gmail.com>
2024-07-11 19:25:10 +0530

    google microsoft

--- ngo_final_frontend/src/app/(pages)/summary/page.js
+++ ngo_final_frontend/src/app/(pages)/summary/page.js
@@ -307,7 +307,7 @@ export default function Page() {
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "");
+              })(window, document, "clarity", "script", "n5pn6g3m20");
             `}
           </Script>
           {/* hotjar */}
