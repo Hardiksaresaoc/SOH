f5542c281621b88c4ccf402997ae1ae75874ca56
hardik <hardiksaresa.oc@gmail.com>
2024-07-11 18:18:52 +0530

    google

--- ngo_final_frontend/src/app/(pages)/summary/page.js
+++ ngo_final_frontend/src/app/(pages)/summary/page.js
@@ -301,6 +301,18 @@ export default function Page() {
             gtag('config', 'G-2LF0J7SXR5');
           `}
           </Script>
+          <Script id="hotjar-script" strategy="afterInteractive">
+            {`
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:5056041,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}
+          </Script>
         </>
       }
 
