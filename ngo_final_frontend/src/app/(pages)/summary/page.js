5bc02e08cdc4d5162d8a3f532c1c8adde4b3eb8a
hardik <hardiksaresa.oc@gmail.com>
2024-07-12 13:31:56 +0530

    email from Sir updated

--- ngo_final_frontend/src/app/(pages)/summary/page.js
+++ ngo_final_frontend/src/app/(pages)/summary/page.js
@@ -290,7 +290,7 @@ export default function Page() {
         <>
           <Script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=G-2LF0J7SXR5"
+            src="https://www.googletagmanager.com/gtag/js?id=G-JNQJMTEQ2D"
           ></Script>
           <Script>
             {`
@@ -298,7 +298,7 @@ export default function Page() {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'G-2LF0J7SXR5');
+            gtag('config', 'G-JNQJMTEQ2D');
           `}
           </Script>
           <Script>
@@ -307,7 +307,7 @@ export default function Page() {
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "n5pn6g3m20");
+              })(window, document, "clarity", "script", "n63r3tps7w"); 
             `}
           </Script>
           {/* hotjar */}
